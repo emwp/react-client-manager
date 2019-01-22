48837b57a209af21c7d7b5e39639ac78f9acf12a
Everton Pereira <everton.mwp@gmail.com>
2019-01-22 13:37:09 -0200

    set up user register to firebase

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -12,18 +12,23 @@ class Login extends Component {
     password: '',
   };
 
+  componentWillMount() {
+    const { allowRegistration } = this.props.settings;
+    if (!allowRegistration) {
+      this.props.history.push('/');
+    }
+  }
+
   onSubmit = e => {
     e.preventDefault();
 
     const { firebase, notifyUser } = this.props;
     const { email, password } = this.state;
 
+    // Register with firebase
     firebase
-      .login({
-        email,
-        password,
-      })
-      .catch(err => notifyUser('Invalid Credentials', 'error'));
+      .createUser({ email, password })
+      .catch(err => notifyUser('This user already exists', 'error'));
   };
 
   onChange = e => this.setState({ [e.target.name]: e.target.value });
@@ -38,7 +43,7 @@ class Login extends Component {
               {message ? <Alert message={message} messageType={messageType} /> : null}
               <h1 className="text-center pb-4 pt-3">
                 <span className="text-primary">
-                  <i className="fas fa-lock" /> Login
+                  <i className="fas fa-lock" /> Register
                 </span>
               </h1>
               <form onSubmit={this.onSubmit}>
@@ -64,7 +69,7 @@ class Login extends Component {
                     onChange={this.onChange}
                   />
                 </div>
-                <input type="submit" value="Login" className="btn btn-primary btn-block" />
+                <input type="submit" value="Register" className="btn btn-primary btn-block" />
               </form>
             </div>
           </div>
@@ -85,6 +90,7 @@ export default compose(
   connect(
     (state, props) => ({
       notify: state.notify,
+      settings: state.settings,
     }),
     { notifyUser },
   ),
